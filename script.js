b19afeaa5ad396fdd7d8119b1d8a7353ca41d0dc
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-07-02 20:27:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-//your JS code here. If required.
 let allCircles=document.querySelectorAll(".circle");
 const next=document.querySelector("#next");
 let idStr="circle-"
 let coun=1;
+
 next.addEventListener("click",()=>{
+
 if(coun<5){
     coun++;
     let circ=document.querySelector(`#${idStr+coun}`)
     circ.classList.add("active");
 }
+updateButtons()
 })
 let prev=document.querySelector("#prev");
+
+
+function updateButtons() {
+    if (coun === 5) {
+        next.classList.add("disable");
+    } else {
+        next.classList.remove("disable");
+    }
+
+    if (coun === 1) {
+        prev.classList.add("disable");
+    } else {
+        prev.classList.remove("disable");
+    }
+}
+
+// Initial update of buttons based on the starting value of coun
+updateButtons();
 prev.addEventListener("click",()=>{
   
     let circ=document.querySelector(`#${idStr+coun}`)
@@ -18,4 +38,5 @@ prev.addEventListener("click",()=>{
         coun--;
     circ.classList.remove("active");
     }
+    updateButtons()
 })
