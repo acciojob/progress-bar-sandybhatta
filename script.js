308624b18415816b76dc360bc999bf5b9ab259b8
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-07-02 20:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let allCircles=document.querySelectorAll(".circle");
+const next=document.querySelector("#next");
+let idStr="circle-"
+let coun=1;
+next.addEventListener("click",()=>{
+if(coun<5){
+    coun++;
+    let circ=document.querySelector(`#${idStr+coun}`)
+    circ.classList.add("active");
+}
+})
+let prev=document.querySelector("#prev");
+prev.addEventListener("click",()=>{
+  
+    let circ=document.querySelector(`#${idStr+coun}`)
+    if(coun>1){
+        coun--;
+    circ.classList.remove("active");
+    }
+})
